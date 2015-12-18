7c0fa85abc12edc7b1a14224fd2c4c5e976c04de
Topher Bullock <cbullock@pivotal.io>
2015-12-18 16:18:13 -0500

    Adds method splitting to runner methods

--- base_app/runner.js
+++ base_app/runner.js
@@ -3,13 +3,43 @@ var app = require("./app.js");
 // Load the http module to create an http server.
 var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
   
-  app(request, response, function() {
-  	response.end();	
-  }); 
+  switch (req.method) {
+  	 case "POST":
+
+  	 	var body = "";
+  	 	
+        req.on('data', function (data) {
+	        body += data;
+	        console.log("Partial body: " + body);
+	    });
+
+	    req.on('end', function () {
+	        try {
+	        	req.JSONBody = JSON.parse(body);	
+	        } catch (e) {
+				res.end();
+	        }
+
+	        app.post(req, res, function() {
+  				res.end();	
+  			});
+	    });
+
+        
+
+        break;
+    case "get":
+    default:
+        app.get(req, res, function() {
+			res.end();	
+		});
+  }
+  
+
+ 
 
 });
 
